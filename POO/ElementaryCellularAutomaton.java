c00cc2f3943ada2c1cbd9ff920de4c88606fdc86
David <42324150+deividgdt@users.noreply.github.com>
2021-03-16 14:58:20 +0100

    stateTable

--- POO/ElementaryCellularAutomaton.java
+++ POO/ElementaryCellularAutomaton.java
@@ -1,10 +1,10 @@
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Model a 1D elementary cellular automaton.
  * 
  * @author David J. Barnes and Michael Kölling
- * @version  2016.02.29 - version 1
+ * @version  2016.02.29 - version 4
  */
 public class Automaton
 {
@@ -12,7 +12,9 @@ public class Automaton
     private final int numberOfCells;
     // The state of the cells.
     private int[] state;
-
+    // The state table encoding the next-state changes.
+    private int[] stateTable;
+    
     /**
      * Create a 1D automaton consisting of the given number of cells.
      * @param numberOfCells The number of cells in the automaton.
@@ -20,28 +22,28 @@ public class Automaton
     public Automaton(int numberOfCells)
     {
         this.numberOfCells = numberOfCells;
+        // Allow an extra element to avoid 'fencepost' errors.
         state = new int[numberOfCells + 1];
-        // Seed the automaton with a single 'on' cell in the middle.
+        stateTable = new int[] {
+            //0, 1, 0, 0, 1, 0, 0, 1, // Wolfram code 146
+            1, 0, 1, 1, 0, 1, 1, 1, // Wolfram code 62
+        };
+        // Seed the automaton with a single 'on' cell.
         state[numberOfCells / 2] = 1;
     }
-
+    
     /**
      * Print the current state of the automaton.
      */
     public void print()
     {
         for(int cellValue : state) {
-            if(cellValue == 1) {
-                System.out.print("⬛");
-            }
-            else {
-                System.out.print(" ");
-            }
+            System.out.print(cellValue == 1 ? "*" : " ");
         }
         System.out.println();
     }   
-
-     /**
+    
+    /**
      * Update the automaton to its next state.
      */
     public void update()
@@ -61,28 +63,43 @@ public class Automaton
         state = nextState;
     }
     
-
     /**
      * Reset the automaton.
      */
     public void reset()
     {
         Arrays.fill(state, 0);
-        // Seed the automaton with a single 'on' cell.
+        // Seed the automaton with a single 'on' cell in the middle.
         state[numberOfCells / 2] = 1;
-
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y
+     * Calculate the next state of the center cell
+     * given current left, center and right cell
+     * values.
+     * This implements Wolfram code 110.
+     * @see https://en.wikipedia.org/wiki/Wolfram_code
+     * @param left The state of the cell to the left of center.
+     * @param center The state of the center cell.
+     * @param right The state of the cell to the right of center.
+     * @return The new value of center (0 or 1).
      */
-    public int calculateNextState(int left, int center, int right)
+    private int calculateNextState(int left, int center, int right)
     {
-        // put your code here
-        return (left + center + right) % 2;
+        return stateTable[encodeTriplet(left, center, right)];
+    }
+    
+    /**
+     * Encode the 1/0 triplet (left, center, right) as an
+     * integer value in the range 0-7.
+     * @param left The state of the cell to the left of center (0 or 1).
+     * @param center The state of the center cell (0 or 1).
+     * @param right The state of the cell to the right of center (0 or 1).
+     * @return (left,center,right) interpreted as a 3-bit value.
+     */
+    private int encodeTriplet(int left, int center, int right)
+    {
+        return left * 4 + center * 2 + right;
     }
 
 }
