51d669b846d4347c33d545056d03dbd3f99bd91a
David <42324150+deividgdt@users.noreply.github.com>
2021-03-16 13:44:16 +0100

    Create ElementaryCellularAutomaton.java

---
+++ POO/ElementaryCellularAutomaton.java
@@ -0,0 +1,88 @@
+import java.util.Arrays;
+
+/**
+ * Model a 1D elementary cellular automaton.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version  2016.02.29 - version 1
+ */
+public class Automaton
+{
+    // The number of cells.
+    private final int numberOfCells;
+    // The state of the cells.
+    private int[] state;
+
+    /**
+     * Create a 1D automaton consisting of the given number of cells.
+     * @param numberOfCells The number of cells in the automaton.
+     */
+    public Automaton(int numberOfCells)
+    {
+        this.numberOfCells = numberOfCells;
+        state = new int[numberOfCells + 1];
+        // Seed the automaton with a single 'on' cell in the middle.
+        state[numberOfCells / 2] = 1;
+    }
+
+    /**
+     * Print the current state of the automaton.
+     */
+    public void print()
+    {
+        for(int cellValue : state) {
+            if(cellValue == 1) {
+                System.out.print("⬛");
+            }
+            else {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }   
+
+     /**
+     * Update the automaton to its next state.
+     */
+    public void update()
+    {
+        // Build the new state in a separate array.
+        int[] nextState = new int[state.length];
+        // Use 0 for the non-existent value to the left of
+        // the first cell.
+        int left = 0;
+        int center = state[0];
+        for(int i = 0; i < numberOfCells; i++) {
+            int right = state[i + 1];
+            nextState[i] = calculateNextState(left, center, right);
+            left = center;
+            center = right;
+        }
+        state = nextState;
+    }
+    
+
+    /**
+     * Reset the automaton.
+     */
+    public void reset()
+    {
+        Arrays.fill(state, 0);
+        // Seed the automaton with a single 'on' cell.
+        state[numberOfCells / 2] = 1;
+
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y
+     */
+    public int calculateNextState(int left, int center, int right)
+    {
+        // put your code here
+        return (left + center + right) % 2;
+    }
+
+}
